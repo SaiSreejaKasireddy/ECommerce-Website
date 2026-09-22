2c5727ccaa2b1a41bf9cc7a93d49e6b0560a8444
SaiSreejaKasireddy <kasireddysaisreeja@gmail.com>
2026-09-22 22:32:03 +0530

    chore: initialize e-commerce project

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const db=require("./src/config/db");
+const app=express();
+const PORT=5000;
+async function startServer(){
+    try{
+        const connection=await db.promise().getConnection();
+        console.log("MySQL connected successfully");
+        connection.release();
+        app.listen(PORT,()=>{
+            console.log(`Server running on http://localhost:${PORT}`);
+        });
+    }
+    catch(error){
+        console.error("MySql connection failed:",error.message);
+    }
+}
+app.get("/api/health", (req, res) => {
+    res.json({
+        success: true,
+        message: "E-Commerce API is running"
+    });
+});
+startServer();
